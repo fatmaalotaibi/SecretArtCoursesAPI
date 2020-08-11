08db61e8b6eabcc6f74dac6e75f3578e1e985801
FN Otaibi <nsayemlil@hotmail.com>
2020-08-11 18:14:55 +0300

    fixed error

--- middleware/passport.js
+++ middleware/passport.js
@@ -1,21 +1,19 @@
 const LocalStrategy = require("passport-local").Strategy;
 const bcrypt = require("bcrypt");
 
+//Models
 const User = require("../db/models/User");
 
 exports.localStrategy = new LocalStrategy(async (username, password, done) => {
   try {
     const user = await User.findOne({
-      where: { username }, // equivalent to { username : username }
+      where: { username: username }, // equivalent to { username : username }
     });
     const passwordsMatch = user
       ? await bcrypt.compare(password, user.password)
       : false;
 
-    if (passwordsMatch) {
-      return done(null, user);
-    }
-    return done(null, false);
+    passwordsMatch ? done(null, user) : done(null, false);
   } catch (error) {
     done(error);
   }
